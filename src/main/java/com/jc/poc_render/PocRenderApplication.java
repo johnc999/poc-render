4869b737b692a260cf828f75169bda47ec04d2f5
John Cooney <johnacooney@yahoo.com>
2025-01-25 09:17:51 +1000

    Initial commit

---
+++ src/main/java/com/jc/poc_render/PocRenderApplication.java
@@ -0,0 +1,13 @@
+package com.jc.poc_render;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocRenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocRenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jc/poc_render/PocRenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.poc_render;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocRenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
